19ecdef2748e228b8f5be06176e38b4c4bd9b7c1
apoorvasa03 <apoorvasa03@gmail.com>
2021-10-14 18:16:30 -0700

    - username change

--- datastructure/chapter2:linkedList/2.05-sumLists.js
+++ datastructure/chapter2:linkedList/2.05-sumLists.js
@@ -1,17 +1,53 @@
 function sumList(list1, list2){
+    let finalList,carry = 0;
+
     while(list1 || list2){
-        let sum = sumListData(list1.data, list2.data)
-        console.log(sum)
+        let sum = addData(list1.data, list2.data, carry)
+        carry = sum.carry
+        if(!finalList){
+            finalList = {data: sum.data, next:{}}
+        }else{
+            finalList = { data: sum.data, next: finalList}
+        }
         list1 = list1.next
         list2 = list2.next
     }
+
+    if(carry) finalList = { data: carry, next: finalList}
+    return finalList
 }
 
-const sumListData = (l1, l2) => {
-    let result = l1 + l2;
-    result = (result+ '').split('').map(i => { return parseInt(i)})
-    if(result.length > 1) return {data: result[1], carry : result[0]}
-    else return {data: result[0], carry: 0}
+const addData = (l1, l2, carry) => {
+    let result = l1 + l2 + carry;
+    return {data: (result % 10), carry: Math.floor(result/10)}
+}
+
+//result = (result+ '').split('').map(i => { return parseInt(i)})
+//if(result.length > 1) return {data: result[1], carry : result[0]}
+//else return {data: result[0], carry: 0}
+
+
+
+//------------ recursion
+
+function sumListsRecursive(list1, list2, carry){
+    if(!list1 && !list2 && carry) return null
+    let finalList = {}
+
+    if(list1) carry += list1.data
+    if(list2) carry += list2.data
+    finalList.data = carry % 10
+
+    if(list1 || list2) {
+        const nextNode = sumListsRecursive(
+            list1 ? list1.next : null,
+            list2 ? list2.next: null,
+            carry >= 10 ? 1: 0
+
+        )
+        finalList = {data: nextNode, next: finalList}
+    }
+    return finalList
 }
 
 
@@ -35,4 +71,4 @@ let listTwo = {
     }
 }
 
-console.log(sumList(listOne, listTwo))
+console.log(JSON.stringify(sumListsRecursive(listOne, listTwo, 0)))
