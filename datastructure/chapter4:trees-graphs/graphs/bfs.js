a979d39c62d021732bc591a54bd70f51974aadb4
apoorvasa03 <apoorvasa03@gmail.com>
2021-10-29 14:48:07 -0700

    - graph

---
+++ datastructure/chapter4:trees-graphs/graphs/bfs.js
@@ -0,0 +1,45 @@
+function bfs(graph, root){
+    let nodesLen = {}
+    for(let i=0; i< graph.length; i++){
+        nodesLen[i] = Infinity;
+    }
+    nodesLen[root] = 0
+
+    let queue = [root];
+    let current;
+
+    while (queue.length != 0) {
+        current = queue.shift();
+        console.log('--------------', current)
+        
+        var curConnected = graph[current];
+        console.log('"" curConnected', curConnected)
+        var neighborIdx = []; 
+        var idx = curConnected.indexOf(1); 
+        console.log('**', idx)
+        while (idx != -1) {
+          neighborIdx.push(idx); 
+          idx = curConnected.indexOf(1, idx + 1); 
+        }
+        console.log('**', neighborIdx)
+        
+        for (var j = 0; j < neighborIdx.length; j++) {
+          if (nodesLen[neighborIdx[j]] == Infinity) {
+            nodesLen[neighborIdx[j]] = nodesLen[current] + 1;
+            queue.push(neighborIdx[j]); 
+          }
+        }
+        console.log('.. nodesLen', nodesLen)
+      }
+      return nodesLen;
+  };
+
+let exBFSGraph =[
+    [0, 1, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0]
+]
+
+console.log(bfs(exBFSGraph, 1))
