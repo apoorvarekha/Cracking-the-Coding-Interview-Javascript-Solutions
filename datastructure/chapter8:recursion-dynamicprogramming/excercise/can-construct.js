7a72f1525efb32dc541ccea74305750861f1da7c
apoorvasa03 <apoorvasa03@gmail.com>
2021-11-09 16:11:07 -0800

    - construct DP

---
+++ datastructure/chapter8:recursion-dynamicprogramming/excercise/can-construct.js
@@ -0,0 +1,42 @@
+// time - o(n * m^2)  space - o(m^2)
+// taget = m  wordbank = n
+function canConstruct(target, wordBank, memo= {}){
+    if(target in memo) return memo[target]
+    if(target === "") return true
+
+    for(let word of wordBank){
+        if(target.indexOf(word) === 0){ // if u find that word in starting 
+            let suffix = target.slice(word.length)
+            if(canConstruct(suffix, wordBank, memo) === true) {
+                memo[target] = true
+                return true
+            }
+        } 
+    }
+    memo[target] = false
+    return false
+}
+
+
+// time - o(n^m * m)  space - o(m^2)
+// taget = m  wordbank = n
+function canConstructWithoutMemo(target, wordBank){
+    if(target === "") return true
+
+    for(let word of wordBank){
+        if(target.indexOf(word) === 0){ // if u find that word in starting 
+            let suffix = target.slice(word.length)
+            if(canConstruct(suffix, wordBank) === true) {
+                return true
+            }
+        } 
+    }
+
+    return false
+}
+
+
+console.log(canConstruct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd'])) // true
+console.log(canConstruct("skateboard", ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])) //false
+console.log(canConstruct("enterapotentpot", ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])) //true
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])) // false
--- datastructure/chapter8:recursion-dynamicprogramming/excercise/fibonacci.js
+++ datastructure/chapter8:recursion-dynamicprogramming/excercise/fibonacci.js
@@ -1,3 +1,9 @@
+// write a function 'fib(n)' that takes in the number of argument, the function should return the nth number of Fibonacci sequence
+// the 0th number of sequence is 0
+// the 1st number of sequence is 1
+// to generate the next number of the sequence, we sum the previous two   
+
+
 
 // time - o(2^n)  space - o(n)
 function fib(n){
