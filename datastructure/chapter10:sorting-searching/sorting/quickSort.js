06d8d3c708e54f9fc4d2a97394121561ca2de7af
apoorvasa03 <apoorvasa03@gmail.com>
2021-11-14 13:19:27 -0800

    - quick sort

--- datastructure/chapter10:sorting-searching/sorting/quickSort.js
+++ datastructure/chapter10:sorting-searching/sorting/quickSort.js
@@ -1,23 +1,16 @@
 /** time- o(n logn)  space - o(n)*/
 function quickSort(array){
-    if(array.length === 1) return array
+    if(array.length <= 1) return array
     // we are taking last element in array(we can also take 1st element)
     let middle = array[array.length -1];  
     let leftArray = []
     let rightArray = []
     // we dont need to consider the middle element
     for(let element of array.slice(0, array.length - 1)){
-        if(element < middle) leftArray.push(element)
-        if(element > middle) rightArray.push(element)
+        element < middle ? leftArray.push(element) : rightArray.push(element)
     };
-    if(leftArray.length && rightArray.length){
-        return [...quickSort(leftArray), middle, ...quickSort(rightArray)]
-    }else if(leftArray.length){
-        return [...quickSort(leftArray), middle]
-    }else{
-        return [middle, ...quickSort(rightArray)]
-    }
     
+    return [...quickSort(leftArray), middle, ...quickSort(rightArray)]
 }
 
 console.log(quickSort([5, 9,4,6,2,10,11, 1, 3]))
